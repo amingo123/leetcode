ca14dbeb842495b5c5a59375e2a879705693ce68
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2019-12-11 12:51:24 +0800

    initial

---
+++ LeetCode/TwoSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Utility;
+
+namespace TwoSum
+{
+    class Program: BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { };
+            int[] r = Solution.TwoSum(nums, 9);
+            PrintArray(r);
+            Read();
+        }
+    }
+
+    public static class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            return null;
+        }
+    }
+}
---
+++ LeetCode/Utility/BaseProgram.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    public class BaseProgram
+    {
+        public static void Write(object o)
+        {
+            Console.Write(o);
+        }
+
+        public static void WriteL(object o)
+        {
+            Console.WriteLine(o);
+        }
+
+        public static void Read()
+        {
+            Console.Read();
+        }
+
+        public static void PrintArray<T>(T[] t)
+        {
+            foreach (var item in t)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
