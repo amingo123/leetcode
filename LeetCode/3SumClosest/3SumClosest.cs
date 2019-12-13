f595f29a6cf40a5bc9dfb3449cecc9d3be7cb994
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2019-12-13 14:46:19 +0800

    3sumclosest over

--- LeetCode/3SumClosest/3SumClosest.cs
+++ LeetCode/3SumClosest/3SumClosest.cs
@@ -9,7 +9,7 @@ namespace _3SumClosest
     {
         static void Main(string[] args)
         {
-            Write(Solution.ThreeSumCloset(nums2, 1));
+            Write(Solution.ThreeSumCloset(nums2, -59));
             Read();
         }
     }
@@ -18,67 +18,85 @@ namespace _3SumClosest
     {
         public static int ThreeSumCloset(int[] nums,int target)
         {
-            List<IList<int>> list = new List<IList<int>>();
             int length = nums.Length;
+            int returnvalue = int.MaxValue;
             if (nums == null || length < 3)
             {
-                return 0;
+                return returnvalue;
             }
             List<int> templist = nums.ToList();
             templist.Sort();
             nums = templist.ToArray();
-            List<int> subList = new List<int>();
-            int ans = 0, current = int.MinValue, leftValue = int.MinValue, rightValue = int.MaxValue;
+
+            int result = 0, leftValue = int.MinValue, rightValue = int.MaxValue;
             int leftIndex, rightIndex;
+            int gap = int.MaxValue;
+            int maxInRange, minInRange;
             for (int i = 0; i < length - 2; i++)
             {
                 if ((i > 0) && (nums[i] == nums[i - 1]))
                 {
                     continue;
                 }
+
                 leftIndex = i + 1;
                 rightIndex = length - 1;
-                current = nums[i];
-                while (leftIndex < rightIndex)
+                maxInRange = nums[i] + nums[rightIndex] + nums[rightIndex - 1];
+                minInRange = nums[i] + nums[leftIndex] + nums[leftIndex + 1];
+
+                if (maxInRange < target && Math.Abs(maxInRange - target) < Math.Abs(gap))
                 {
-                    leftValue = nums[leftIndex];
-                    rightValue = nums[rightIndex];
-                    ans = current + leftValue + rightValue;
-                    if (ans == 0)
+                    gap = maxInRange - target;
+                    returnvalue = maxInRange;
+                }
+                else if (minInRange > target && Math.Abs(minInRange - target) < Math.Abs(gap))
+                {
+                    gap = minInRange - target;
+                    returnvalue = minInRange;
+                }
+                else
+                {
+                    while (leftIndex < rightIndex)
                     {
-                        // 3者和为 0, 插入 list 数据
-                        subList = new List<int>
+                        leftValue = nums[leftIndex];
+                        rightValue = nums[rightIndex];
+                        result = nums[i] + leftValue + rightValue;
+                        if (result == target)
                         {
-                            current,
-                            leftValue,
-                            rightValue
-                        };
-                        list.Add(subList);
-                        // right--, left++ 去除重复值
-                        while (rightValue == nums[--rightIndex] && leftIndex < rightIndex)
-                        {
+                            return result;
                         }
-                        while (leftValue == nums[++leftIndex] && leftIndex < rightIndex)
+                        else if (result > target)
                         {
+                            if (Math.Abs(gap) > Math.Abs(result - target))
+                            {
+                                gap = result - target;
+                                returnvalue = result;
+                            }
+
+                            if (nums[i] > target && nums[i] >= 0) return returnvalue;
+
+                            // 和大于 0，多了最大值--，right-- 去除重复值
+                            while (rightValue == nums[--rightIndex] && leftIndex < rightIndex)
+                            {
+                            }
                         }
-                    }
-                    else if (ans > 0)
-                    {
-                        // 和大于 0，多了最大值--，right-- 去除重复值
-                        while (rightValue == nums[--rightIndex] && leftIndex < rightIndex)
+                        else
                         {
-                        }
-                    }
-                    else
-                    {
-                        // 和小于 0，少了最小值++，left-- 去除重复值
-                        while (leftValue == nums[++leftIndex] && leftIndex < rightIndex)
-                        {
+                            if (Math.Abs(gap) > Math.Abs(result - target))
+                            {
+                                gap = result - target;
+                                returnvalue = result;
+                            }
+
+                            // 和小于 0，少了最小值++，left-- 去除重复值
+                            while (leftValue == nums[++leftIndex] && leftIndex < rightIndex)
+                            {
+                            }
                         }
                     }
                 }
             }
-            return 0;
+            return returnvalue;
         }
     }
 }
