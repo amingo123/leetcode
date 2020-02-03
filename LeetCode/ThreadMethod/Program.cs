ccf31e45dd44115d8a927075e072616ea2ea1a80
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2020-02-03 15:03:52 +0800

    ThreadMethod

---
+++ LeetCode/ThreadMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using Utility;
+
+namespace ThreadMethod
+{
+    class Program : BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        static void ThreadWithCallBack(ThreadStart threadStart, Action action)
+        {
+            ThreadStart ts = new ThreadStart(() =>
+            {
+                threadStart();
+                action();
+            });
+            Thread thread = new Thread(ts);
+            thread.Start();
+        }
+
+        static Func<T> ThreadWithReturn<T>(Func<T> func)
+        {
+            T t = default(T);
+            Thread thread = new Thread(
+                new ThreadStart(() =>
+                {
+                    t = func();
+                })
+             );
+
+            thread.Start();
+
+            return new Func<T>(() =>
+            {
+                thread.Join();
+                return t;
+            });
+        }
+    }
+}
