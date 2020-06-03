6e71517ddf9e5aedc2df3f015c1b669813b8ace8
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2020-06-03 13:47:25 +0800

    100

---
+++ LeetCode/100. SameTree/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Utility;
+
+namespace _100._SameTree
+{
+    class Program:BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            TreeNode tn2 = new TreeNode(5);
+            TreeNode tn3 = new TreeNode(15);
+            TreeNode tn1 = new TreeNode(10, tn2, tn3);
+
+            TreeNode tn15 = new TreeNode(15);
+            TreeNode tn12 = new TreeNode(5,null, tn15);
+            TreeNode tn11 = new TreeNode(10, tn12);
+            var i = Solution.IsSameTree(tn1, tn11);
+            Console.WriteLine(i);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Solution
+    {
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if (p == null || q == null) return false;
+            if (p == q) return true;
+            if (p.val != q.val) return false;
+            var b1 = IsSameTree(p.left, q.left);
+            var b2 = IsSameTree(p.right, q.right);
+            return b1 && b2;
+        }
+    }
+}
---
+++ LeetCode/563. BinaryTreeTilt/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Utility;
+
+namespace _563._BinaryTreeTilt
+{
+    class Program: BaseProgram
+    {
+        static void Main(string[] args)
+        {            
+            TreeNode tn2 = new TreeNode(2);
+            TreeNode tn3 = new TreeNode(3);
+            TreeNode tn1 = new TreeNode(1, tn2, tn3);
+            int i = Solution.FindTilt(tn1);
+            Console.WriteLine(i);
+
+            tn2 = new TreeNode(2);
+            tn1 = new TreeNode(1, tn2);
+            i = Solution.FindTilt(tn1);
+            Console.WriteLine(i);
+
+            TreeNode tn5 = new TreeNode(5);
+            tn3 = new TreeNode(3,tn5);
+            TreeNode tn4 = new TreeNode(4);
+            tn2 = new TreeNode(2,tn4);
+            tn1 = new TreeNode(1, tn2,tn3);
+            i = Solution.FindTilt1(tn1);
+            Console.WriteLine(i);
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static class Solution
+    {
+        private static int tiltSum;
+
+        public static int FindTilt1(TreeNode root)
+        {
+            tiltSum = 0;
+            DFS(root);
+            return tiltSum;
+        }
+
+        private static int DFS(TreeNode node)
+        {
+            if (node == null)
+                return 0;
+            int sumLeft = DFS(node.left);
+            int sumRight = DFS(node.right);
+            tiltSum += Math.Abs(sumLeft - sumRight);
+            return node.val + sumLeft + sumRight;
+        }
+
+        public static int FindTilt(TreeNode root)
+        {
+            if (root == null) return 0;
+            
+            if (root.left == null && root.right == null)
+            {
+                return root.val;
+            }
+            int left = FindTilt(root.left);
+            int right = FindTilt(root.right);
+            return Math.Abs(left - right);
+        }
+    }
+}
