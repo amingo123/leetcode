4561dbd1e46e5e22f02a277a389cdfbcc67706ca
Jeffrey Zhou <jeffrey.zhou@hexagon.com>
2020-06-05 14:09:21 +0800

    51

---
+++ LeetCode/46. Permutations/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Utility;
+
+namespace _46._Permutations
+{
+    class Program : BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3 };
+            var i = Solution.Permute(a);
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public static class Solution
+    {
+        static readonly IList<IList<int>> list = new List<IList<int>>();
+        static readonly List<int> track = new List<int>();
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return list;
+            Backtrack(nums);
+            return list;
+        }
+
+        // 路径：记录在 track 中
+        // 选择列表：nums 中不存在于 track 的那些元素
+        // 结束条件：nums 中的元素全都在 track 中出现
+        static void Backtrack(int[] nums)
+        {
+            // 触发结束条件
+            if (track.Count == nums.Length)
+            {
+                list.Add(new List<int>(track));
+                return;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                // 排除不合法的选择
+                if (track.Contains(nums[i])) continue;
+                // 做选择
+                track.Add(nums[i]);
+                // 进入下一层决策树
+                Backtrack(nums);
+                // 取消选择
+                track.RemoveAt(track.Count - 1);
+            }
+        }
+    }
+}
---
+++ LeetCode/51. N-Queens/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Utility;
+
+namespace _51._N_Queens
+{
+    class Program : BaseProgram
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            IList<IList<string>> a = s.SolveNQueens(4);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        readonly IList<IList<string>> list = new List<IList<string>>();
+        readonly List<string> track = new List<string>();
+        const char EMPTY = '.';
+        const char QUEUE = 'Q';
+        private int N;
+        public IList<IList<string>> SolveNQueens(int n)
+        {
+            if (n == 0) return list;
+            N = n;
+            Backtrack(0);
+            return list;
+        }
+
+        // 路径：board 中小于 row 的那些行都已经成功放置了皇后
+        // 选择列表：第 row 行的所有列都是放置皇后的选择
+        // 结束条件：row 超过 board 的最后一行
+        void Backtrack(int row)
+        {
+            // 触发结束条件
+            if (row == N)
+            {
+                list.Add(new List<string>(track));
+                return;
+            }
+
+            for (int col = 0; col < N; col++)
+            {
+                // 排除不合法选择
+                if (!isValid(row, col)) continue;
+                var s = Enumerable.Repeat(EMPTY,N).ToArray();
+                s[col] = QUEUE;
+                track.Add(new string(s));
+                Backtrack(row + 1);
+                track.RemoveAt(track.Count - 1);
+            }
+        }
+
+        /// <summary>
+        /// WRONG
+        /// </summary>
+        /// <param name="row"></param>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        bool isValid(int row, int col)
+        {
+            if (track == null || track.Count == 0) return true;
+            // 检查列是否有皇后互相冲突
+            for (int i = 0; i < track.Count; i++)
+            {
+                if (track[i][col] == 'Q')
+                    return false;
+            }
+            // 检查右上方是否有皇后互相冲突
+            for (int i = row - 1, j = col + 1;
+                    i >= 0 && j < track.Count; i--, j++)
+            {
+                if (track[i][j] == 'Q')
+                    return false;
+            }
+            // 检查左上方是否有皇后互相冲突
+            for (int i = row - 1, j = col - 1;
+                    i >= 0 && j >= 0; i--, j--)
+            {
+                if (track[i][j] == 'Q')
+                    return false;
+            }
+            return true;
+        }
+    }
+}
